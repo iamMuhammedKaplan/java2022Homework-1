75e7b15243841bc728b3bebcf2418eaa5920b168
iamMuhammedKaplan <kpln12345@gmail.com>
2022-10-11 18:38:59 +0300

    variables
    yt engin demirog java2022 hw-1

---
+++ variables/src/Main.java
@@ -0,0 +1,19 @@
+
+public class Main {
+
+	public static void main(String[] args) {
+		//Java has case sensitivity
+		
+		//camel casing
+		int ogrenciSayisi = 12;
+		String mesaj = "Öðrenci sayýsý : ";
+		
+		System.out.println(mesaj + ogrenciSayisi);
+		System.out.println("Öðrenci sayýsý : " + ogrenciSayisi);
+		System.out.println("Öðrenci sayýsý : " + ogrenciSayisi);
+		System.out.println("Öðrenci sayýsý : " + ogrenciSayisi);
+		System.out.println("Öðrenci sayýsý : " + ogrenciSayisi);
+		//re-usability
+	}
+
+}
